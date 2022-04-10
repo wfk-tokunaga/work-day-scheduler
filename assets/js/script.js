dbb8cdaac835af94e8a7eb1c61ea3deefbc449c7
wfk-tokunaga <wktokunaga@gmail.com>
2022-04-10 13:39:32 -0700

    added local storage functionability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,95 @@
-// Add activity
-// Click on the time area, change it to a text input field 
+var schedule = {};
+
+var saveSchedule = function() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+var loadSchedule = function() {
+    var schedule = JSON.parse(localStorage.getItem("schedule"));
+    if (!schedule) {
+        // Initiate schedule
+        schedule = {
+            "9": "",
+            "10": "",
+            "11": "",
+            "12": "",
+            "13": "",
+            "14": "",
+            "15": "",
+            "16": "",
+            "17": "",
+        }
+    }
+
+    // If the schedule does exist already, load it into the table
+
+    console.log($(".time-table .time-table-row"));
+    $(".time-table .time-table-row").each(function(index, rowEl) {
+        var time = $(rowEl).attr("data-time").replace(":00", "");
+        $(rowEl).find(".event-section").text(schedule[time]);
+    });
+}
+
+
+// Add handler to check current time and adjust accordingly
+var auditSchedule = function(timeTableRowEl) {
+    var taskTime = timeTableRowEl.getAttribute("data-time");
+    var taskMoment = moment(taskTime,"hh:mm");
+    var currentMoment = moment();
+    if (currentMoment.isBefore(taskMoment)) {
+        $(timeTableRowEl.children[1]).addClass("bg-success");
+      } else if (currentMoment.diff(taskMoment, "hours") < 1) {
+        $(timeTableRowEl).find(".event-section").addClass("bg-danger");
+      } else {
+        $(timeTableRowEl.children[1]).addClass("bg-secondary");
+      }
+}
 
 // Turn an event-section into an input field
-$(".timeTable").on("click", ".event-section", function () {
+$(".time-table").on("click", ".event-section", function () {
     // Get old value
-    var task = $(this).text();
+    var task = $(this).find("p").text();
     var textInput = $("<textarea>").addClass("form-control").val(task);
-    $(this).replaceWith(textInput);
+    // Change <p> to be a text input already filled in with the old value
+    $(this).find("p").replaceWith(textInput);
     textInput.trigger("focus");
-    console.log(text);
 });
 
+
+// When done with entering text, change it back into the span element or whatever
+$(".time-table").on("blur", "textarea", function () {
+    // Get old value
+    // var taskTime = this.getAttribute("data-time");
+    var task = $(this).val();
+    var taskP = $("<p>").text(task);
+    var taskTime = $(this)
+        .closest(".time-table-row")
+        .attr("data-time")
+        .replace(":00", "");
+    schedule[taskTime] = task; 
+    saveSchedule();
+
+    $(this).replaceWith(taskP);
+    taskP.trigger("focus");
+});
+
+loadSchedule();
+
+$(".time-table .time-table-row").each(function (index, el) {
+    auditSchedule(el);
+});
+
+// Update header date
+$(".dateHeader").text(moment().format("dddd, MMMM Do YYYY"));
+setInterval(function () {
+    $(".dateHeader")
+    .text(moment()
+    .format("dddd, MMMM Do YYYY"));
+  }, 
+  ((1000 * 60) * 60 * 24));
+
+setInterval(function () {
+$(".time-table .time-table-row").each(function (index, el) {
+    auditSchedule(el);
+});
+}, (1000 * 60) * 2);
